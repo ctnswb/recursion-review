fe5c68bf5673e016ca188b9c26e961afd92f2c70
ctnswb <xclai.07@gmail.com>
2017-05-17 16:26:15 -0700

    Implemented stringifyJSON, added fill me in in stringifyJSONSpec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // numbers
+  if (obj === null) {
+    return "null";
+  } else if (typeof obj === 'string') {
+      return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+      var a = [];
+      for (var i = 0 ; i < obj.length ; i++) {
+        a.push(stringifyJSON(obj[i]));
+      }
+      return "[" + a.join() + "]";
+  } else if (typeof obj === 'object') {
+      var o = [];
+      for (var key in obj) {
+        if(typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+          continue;
+        }
+        o.push(stringifyJSON(key) + ':'+ stringifyJSON(obj[key]));
+      }
+      return "{" + o.join() + "}";
+  } else {
+      return '' + obj;
+  }
 };
+
+
