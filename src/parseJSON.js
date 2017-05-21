a3329b31bf3edc473f4575e8bb79b5f7316cd214
ctnswb <xclai.07@gmail.com>
2017-05-20 19:33:39 -0700

    almost completed implementation of parseJSON missing handling of escape values

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -17,8 +17,11 @@ var basicNesting = [
   '{"a":{"b":"c"}}',
   '{"a":["b", "c"]}',
   '[{"a":"b"}, {"c":"d"}]',
-  '{"a":[1, 3],"c": {"aeu":[]}, "b": true}',
-  '[[[["foo"]]]]'
+  '{"a":[],"c": {}, "b": true}',
+  '[[[["foo"]]]]',
+  '{"a":[],"c": {"a": "b"}, "b": true}',
+  '{"a":[],"c": {"a": [1 , 3, 4], "b": "c"}, "b": [{"a":"ethn"}, {"h":"aeua"}]}',
+  '{ "a": "a1", "b" : "b1", "c" : 25, "d" : { "dd": "dd1", "ee" : "ee1", "ff" : "ff1", "gg" :  "gg1" }, "e": [ { "hh" : "hh1", "ii": "4" }, { "jj" : "jjj", "kk": "555-4567" } ] }'
   ];
 
 //escaping
@@ -58,12 +61,14 @@ var everything = [
     '"callback","required":true,"type":"callback"}]}],' +
     '"name":"LockerUploader","version":{"major":0,' +
     '"micro":1,"minor":0},"versionString":"0.0.1"}',
+
   '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
     '25, "address" : { "streetAddress": "21 2nd Street", ' +
     '"city" : "New York", "state" : "NY", "postalCode" : ' +
     ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
     '"number": "212 555-1234" }, { "type" : "fax", ' +
     '"number": "646 555-4567" } ] }',
+
   '{\r\n' +
     '          "glossary": {\n' +
     '              "title": "example glossary",\n\r' +
@@ -91,106 +96,86 @@ var everything = [
   ];
 
 
-var parseJSON = function(json) {
 
- if (json[0] === '{') {
-      var results = {};
-
-      //returns an empty object
-      if (json === '{}'){
-        return results;
-      }
-
-      //find first :
-      var firstColonIndex = json.indexOf(':');
-
-      if (json[firstColonIndex+1] === '{' ){
-        // check for nested object
-        var k = json.substring(2, firstColonIndex-1);
-        var v = json.substring(firstColonIndex+1, json.length-1);
-        results[k] = parseJSON(v);
-      } else if (json[firstColonIndex+1] === '[' && json[json.length-2] === ']') {
-
-        // check for nested array
-        var k = json.substring(2, firstColonIndex-1);
-        var v = json.substring(firstColonIndex+1, json.length-1);
-        results[k] = parseJSON(v);
-      } else {
-        //no nesting here
-        var splitObj = json.substring(1, json.length-1).split(/,"|,\s"/);
-        for (var i = 0 ; i < splitObj.length ; i++) {
-          //split each pair into [key, value]
-          var pair = splitObj[i].split(':');
-          var k;
-
-          var v;
-          //check for first quote
-          if (pair[0][0] === '"'){
-            //start at 1 index
-            k = pair[0].substring(1, pair[0].length-1);
-          } else {
-            //start at 0 index
-            k = pair[0].substring(0, pair[0].length-1);
+var parseJSON = function(json) {  //this function returns an array of the split items to parse
+  function splitOnSeparatingCommas () {
+        var inQuote = false; // false means the character is not inside quotes
+        var inBracket = 0; // value of 0 means the character is not inside a bracket
+        var inCurly = 0; // value of 0 means the character is not inside a curly brace
+        var commas = []; //indexes of all commas to separate json on
+        for (var i = 1 ; i < json.length -1 ; i++) { //don't loop through the first and last {} or [])
+          if (json[i] === ',') { // if the character is a comma
+            if (!inQuote && inBracket === 0 && inCurly === 0 ) {
+              //  if its not inside an expression, add the index to the commas array
+              commas.push(i);
+            }
+          } else if (json[i] === '"') {
+            inQuote = !inQuote;
+          } else if (json[i] === '{') { // open bracket means its going inside
+            inBracket++;
+          } else if (json[i] === '}') { // close bracket means its leaving
+            inBracket--;
+          } else if (json[i] === '[') { //entering curly expression
+            inCurly++;
+          } else if (json[i] === ']') { //leaving curly expression
+            inCurly--;
           }
-          v = pair[1].trim();
-          //console.log('k:'+k);
-          //console.log('v:'+v);
-          results[k] = parseJSON(v);
         }
-
-      }
-
+        var previousCommaIndex = 1;
+        var splitObj = [];
+        var pair = "";
+        for (var i = 0 ; i < commas.length ; i++) {
+          pair = json.substring(previousCommaIndex,commas[i]).trim();
+          splitObj.push(pair);
+          previousCommaIndex = commas[i]+1; //start next index after the comma
+        }
+        pair = json.substring(previousCommaIndex, json.length-1).trim(); // get the last pair to the end of json
+        splitObj.push(pair);
+        return splitObj;
+      } // end of splitOnSeparatingCommas function
+  if (json[0] === '{') {  // if its an object
+    var results = {};
+    if (json === '{}') { //returns an empty object
       return results;
     }
-
-
-
-  // [ array // '[]'
-  if (json[0] === '[') {
-
-    var results = [];
-
-    //returns an empty array
-    if (json === '[]') {
-      return results;
+    var splitObj = splitOnSeparatingCommas();
+    for (var i = 0 ; i < splitObj.length ; i++) { //split each pair into [key, value]
+      var colonIndex = splitObj[i].indexOf(':'); //get index of first :
+      var beforeColon = splitObj[i].substring(0, colonIndex).trim(); // get rid of extra white space before colon
+      var key = beforeColon.substring(1,beforeColon.length-1); // get key from inside quotes
+      var val = splitObj[i].substring(colonIndex+1, splitObj[i].length).trim();
+      results[key] = parseJSON(val);
     }
-
-    var array = json.substring(1, json.length-1).split(',');
-   // console.log('array:'+array);
-    for (var i = 0 ; i < array.length ; i++ ) {
-     //console.log('index'+i+':'+ array[i].trim());
-      results.push(parseJSON(array[i].trim()));
-    }
-    //console.log(results);
     return results;
   }
-
-  // " string
-  else if (json[0] === '"') {
+  if (json[0] === '[') { // [ array // '[]'
+    var results = [];
+    if (json === '[]') { //returns an empty array
+      return results;
+    }
+    var array = splitOnSeparatingCommas();
+    for (var i = 0 ; i < array.length ; i++ ) {
+      results.push(parseJSON(array[i].trim()));
+    }
+    return results;
+  }
+  else if (json[0] === '"') {  // " string
     return json.substring(1,json.length-1);
   }
-
-  // number
-  else if (isNaN(json) === false) {
+  else if (isNaN(json) === false) {    // number
     return + json;
   }
-
-  // null
-  else if (json === 'null'){
+  else if (json === 'null'){ //null
     return null;
   }
-
- // boolean
-  else if (json === 'false'){
+  else if (json === 'false'){ //boolean
     return false;
   }
-
   else if (json === 'true'){
     return true;
   }
-
-
 };
+
 
 
 console.log('Basic Tests:');
@@ -200,7 +185,7 @@ for (var i = 0 ; i < basicTests.length ; i++) {
 
 console.log('Basic Nesting:');
 for (var i = 0 ; i < basicNesting.length ; i++) {
-  //console.log('---------------------------');
+  console.log('---------------------------');
   console.log("test "+i+": "+(JSON.stringify(JSON.parse(basicNesting[i]))===JSON.stringify(parseJSON(basicNesting[i]))));
 }
 
@@ -208,7 +193,6 @@ console.log('Escaping:');
 for (var i = 0 ; i < escaping.length ; i++) {
   console.log("test "+i+": "+(JSON.stringify(JSON.parse(escaping[i]))===JSON.stringify(parseJSON(escaping[i]))));
 }
-
 
 console.log('Everything:');
 for (var i = 0 ; i < everything.length ; i++) {
